c8522fe84472efd34bedc6927edbd2c9f1a6a840
ajkn1904 <ajknishat@gmail.com>
2023-02-19 05:13:40 +0600

    update

--- src/App.js
+++ src/App.js
@@ -24,15 +24,15 @@ function App() {
       "sec_id": '02',
       "sec": [
         {
-          "id": '04',
+          "id": '01',
           "url": 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
         },
         {
-          "id": '05',
+          "id": '02',
           "url": 'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
         },
         {
-          "id": '06',
+          "id": '03',
           "url": 'https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
         }
       ]
@@ -41,15 +41,66 @@ function App() {
       "sec_id": '03',
       "sec": [
         {
-          "id": '07',
+          "id": '01',
           "url": 'https://images.unsplash.com/photo-1475924156734-496f6cac6ec1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
         },
         {
-          "id": '08',
+          "id": '02',
           "url": 'https://images.unsplash.com/photo-1535463731090-e34f4b5098c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
         },
         {
-          "id": '09',
+          "id": '03',
+          "url": 'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q'
+        }
+      ]
+    },
+    {
+      "sec_id": '04',
+      "sec": [
+        {
+          "id": '01',
+          "url": 'https://images.unsplash.com/photo-1586348943529-beaae6c28db9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80'
+        },
+        {
+          "id": '02',
+          "url": 'https://images.unsplash.com/photo-1439853949127-fa647821eba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'
+        },
+        {
+          "id": '03',
+          "url": 'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q'
+        }
+      ]
+    },
+    {
+      "sec_id": '05',
+      "sec": [
+        {
+          "id": '01',
+          "url": 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        },
+        {
+          "id": '02',
+          "url": 'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        },
+        {
+          "id": '03',
+          "url": 'https://images.unsplash.com/photo-1518173946687-a4c8892bbd9f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+        }
+      ]
+    },
+    {
+      "sec_id": '06',
+      "sec": [
+        {
+          "id": '01',
+          "url": 'https://images.unsplash.com/photo-1586348943529-beaae6c28db9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80'
+        },
+        {
+          "id": '02',
+          "url": 'https://images.unsplash.com/photo-1439853949127-fa647821eba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'
+        },
+        {
+          "id": '03',
           "url": 'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q'
         }
       ]
@@ -68,7 +119,7 @@ function App() {
         imgUrl.map(imgSection =>
           
           
-          <div className='column' key={imgSection.sec_id}>
+          <div className='img-column' key={imgSection.sec_id}>
 
             {
               imgSection.sec.map(galleryImg =>
